33d226ce5b7d0d0ae46146592404c86ca4317187
rbulda50 <117113002+rbulda50@users.noreply.github.com>
2023-04-28 21:31:27 +0300

    updated
    changed package.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+import React, {Component} from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
       React homework template
-    </div>
-  );
-};
+    </div>)
+  }
+}
+
+export default App;
